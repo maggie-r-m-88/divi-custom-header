c0737df1f5e457a301e05d9b3d2a2ffd6dccbbfe
Maggie Martin <8229633+maggie-r-m-88@users.noreply.github.com>
2020-05-22 19:02:26 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,15 +2,26 @@
 
 function divi_vertical_widgets(){
 register_sidebar( array(
-	'name' => 'Vertical Nav Widgets',
+	'name' => 'Vertical Nav Toggle Widget',
 	'id' => 'ver-nav',
-	'description' => __('Add widgets to Vertical Menu'),
+	'description' => __('Toggle Button for Vertical Menu'),
 	'before_widget' => '<div id="ver-nav">',
 	'after_widget' => '</div>',
 	'before_title' => false,
 	'after_title' => false
 ) );
+
+register_sidebar( array(
+  'name' => 'Bottom Nav Widget',
+  'id' => 'bottom-nav',
+  'description' => __('Add widgets to Vertical Menu'),
+  'before_widget' => '<div id="bottom-nav">',
+  'after_widget' => '</div>',
+  'before_title' => false,
+  'after_title' => false
+) );
 }
+
 function call_widgets_ver_menu() {
 	
 	$ver_nav = et_get_option( 'vertical_nav');
@@ -20,3 +31,4 @@ function call_widgets_ver_menu() {
 }
 }
 add_filter( 'widgets_init', 'divi_vertical_widgets', 10 );
+//add_action('et_header_top','call_widgets_ver_menu', 10);

b62c3f93fc808705ed948d3c9550b658335fc01d
Maggie Martin <8229633+maggie-r-m-88@users.noreply.github.com>
2020-05-22 19:39:37 +0200

    update widgets init
    old widget was causing a problem for some reason

--- functions.php
+++ functions.php
@@ -2,33 +2,27 @@
 
 function divi_vertical_widgets(){
 register_sidebar( array(
-	'name' => 'Vertical Nav Toggle Widget',
-	'id' => 'ver-nav',
-	'description' => __('Toggle Button for Vertical Menu'),
-	'before_widget' => '<div id="ver-nav">',
+	'name' => 'Vertical Nav Contact Widget',
+	'id' => 'ver-nav-widget',
+	'description' => __('Add bottom widgets to Vertical Menu'),
+	'before_widget' => '<div id="custom-bottom-nav">',
 	'after_widget' => '</div>',
 	'before_title' => false,
 	'after_title' => false
 ) );
 
 register_sidebar( array(
-  'name' => 'Bottom Nav Widget',
-  'id' => 'bottom-nav',
-  'description' => __('Add widgets to Vertical Menu'),
-  'before_widget' => '<div id="bottom-nav">',
+  'name' => 'Vertical Nav Toggle Widget',
+  'id' => 'top-nav-widget',
+  'description' => __('Toggle Button for Vertical Menu'),
+  'before_widget' => '<div id="custom-menu-nav">',
   'after_widget' => '</div>',
   'before_title' => false,
   'after_title' => false
 ) );
+
+
 }
 
-function call_widgets_ver_menu() {
-	
-	$ver_nav = et_get_option( 'vertical_nav');
-	if ($ver_nav == true){
-	$output = dynamic_sidebar('ver-nav');
-	return $output;
-}
-}
+
 add_filter( 'widgets_init', 'divi_vertical_widgets', 10 );
-//add_action('et_header_top','call_widgets_ver_menu', 10);
